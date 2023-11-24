0be931472574e06a486c4c766452baf53b7e2bc5
Beatriz Mendes <beatriz.mendes@camunda.com>
2023-11-24 15:44:53 +0100

    feat: append node

---
+++ test/spec/features/append/AppendNodeProvider.spec.js
@@ -0,0 +1,119 @@
+import {
+  insertCoreStyles,
+  insertBpmnStyles,
+  bootstrapModeler,
+  inject
+} from 'test/TestHelper';
+
+import {
+  query as domQuery
+} from 'min-dom';
+
+import {
+  forEach
+} from 'min-dash';
+
+import Append from 'lib/features/append';
+
+import {
+  CreateAppendAnythingModule
+} from 'bpmn-js-create-append-anything';
+
+import diagramXML from './AppendNode.bpmn';
+
+insertCoreStyles();
+insertBpmnStyles();
+
+describe('<AppendNodeProvider>', function() {
+
+  function bootstrap(diagramXML) {
+
+    return function() {
+      return bootstrapModeler(diagramXML, {
+        additionalModules: [
+          Append,
+          CreateAppendAnythingModule
+        ]
+      })();
+    };
+  }
+
+  beforeEach(bootstrap(diagramXML));
+
+
+  describe('should trigger action', function() {
+
+    it('click', inject(function(elementRegistry, appendNode) {
+
+      // given
+      const shape = elementRegistry.get('Task');
+
+      // when
+      appendNode.show(shape);
+      appendNode.trigger('click', shape);
+
+      // then
+      expect(domQuery('.djs-popup.bpmn-append')).to.exist;
+    }));
+
+    it('dragstart', inject(function(elementRegistry, appendNode, dragging) {
+
+      // given
+      const shape = elementRegistry.get('Task');
+
+      // when
+      appendNode.show(shape);
+      appendNode.trigger('dragstart', shape);
+
+      // then
+      expect(dragging.context().data.context.start).to.eql(shape);
+    }));
+
+  });
+
+
+  describe('#isAllowed', function() {
+
+    it('should return true', inject(function(elementRegistry, appendNodeProvider) {
+
+      // given
+      const shape = elementRegistry.get('Task');
+
+      // when
+      const allowed = appendNodeProvider.isAllowed(shape);
+
+      // then
+      expect(allowed).to.be.true;
+    }));
+
+
+    describe('should return false', function() {
+
+      // given
+      const ids = [
+        'EndEvent',
+        'Group',
+        'TextAnnotation',
+        'Lane',
+        'Participant',
+        'DataStoreReference',
+        'DataObjectReference'
+      ];
+
+      forEach(ids, function(id) {
+
+        it(`should dissalow for element <${id}>`, inject(function(elementRegistry, appendNodeProvider) {
+          const shape = elementRegistry.get(id);
+
+          // when
+          const allowed = appendNodeProvider.isAllowed(shape);
+
+          // then
+          expect(allowed).to.be.false;
+        }));
+      });
+    });
+
+  });
+
+});
