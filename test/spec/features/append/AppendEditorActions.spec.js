8a99c25968f6381347b75afc5cf7b6875a957c40
Beatriz Mendes <beatriz.mendes@camunda.com>
2023-12-20 15:42:00 +0000

    fix(append): add keyboard shortcut
    Closes #14

---
+++ test/spec/features/append/AppendEditorActions.spec.js
@@ -0,0 +1,122 @@
+import {
+  bootstrapModeler,
+  inject,
+  getBpmnJS
+} from 'test/TestHelper';
+
+import {
+  query as domQuery
+} from 'min-dom';
+import Append from 'lib/features/append';
+
+import {
+  CreateAppendAnythingModule
+} from 'bpmn-js-create-append-anything';
+
+import diagramXML from './AppendNode.bpmn';
+
+describe('<AppendEditorActions>', function() {
+
+  beforeEach(bootstrapModeler(diagramXML, {
+    additionalModules: [
+      CreateAppendAnythingModule,
+      Append
+    ]
+  }));
+
+
+  it('should unregister "appendElement"', inject(function(editorActions) {
+
+    // then
+    expect(editorActions.isRegistered('appendElement')).to.be.false;
+  }));
+
+
+  describe('#appendNode', function() {
+
+    it('should open append element', inject(function(elementRegistry, selection, editorActions, eventBus) {
+
+      // given
+      const element = elementRegistry.get('Task');
+
+      selection.select(element);
+      const changedSpy = sinon.spy();
+
+      // when
+      eventBus.once('popupMenu.open', changedSpy);
+
+      editorActions.trigger('appendNode', {});
+
+      // then
+      expect(changedSpy).to.have.been.called;
+      expect(isMenu('bpmn-append')).to.be.true;
+    }));
+
+
+    it('should open create element if multiple elements selected', inject(function(elementRegistry, selection, editorActions, eventBus) {
+
+      // given
+      const elementIds = [ 'StartEvent', 'Task' ];
+      const elements = elementIds.map(function(id) {
+        return elementRegistry.get(id);
+      });
+
+      selection.select(elements);
+      const changedSpy = sinon.spy();
+
+      // when
+      eventBus.once('popupMenu.open', changedSpy);
+
+      editorActions.trigger('appendNode', {});
+
+      // then
+      expect(changedSpy).to.have.been.called;
+      expect(isMenu('bpmn-create')).to.be.true;
+    }));
+
+
+    it('should open create element if no selection', inject(function(editorActions, eventBus) {
+
+      // given
+      const changedSpy = sinon.spy();
+
+      // when
+      eventBus.once('popupMenu.open', changedSpy);
+
+      editorActions.trigger('appendNode', {});
+
+      // then
+      expect(changedSpy).to.have.been.called;
+      expect(isMenu('bpmn-create')).to.be.true;
+    }));
+
+
+    it('should open create element if append not allowed', inject(function(elementRegistry, selection, editorActions, eventBus) {
+
+      // given
+      const element = elementRegistry.get('EndEvent');
+
+      selection.select(element);
+      const changedSpy = sinon.spy();
+
+      // when
+      eventBus.once('popupMenu.open', changedSpy);
+
+      editorActions.trigger('appendNode', {});
+
+      // then
+      expect(changedSpy).to.have.been.called;
+      expect(isMenu('bpmn-create')).to.be.true;
+    }));
+
+  });
+
+});
+
+// helpers //////////////////////
+function isMenu(menuId) {
+  const popup = getBpmnJS().get('popupMenu');
+  const popupElement = popup._current && domQuery('.djs-popup', popup._current.container);
+
+  return popupElement.classList.contains(menuId);
+}
---
+++ test/spec/features/append/AppendKeyboardBindings.spec.js
@@ -0,0 +1,115 @@
+import {
+  bootstrapModeler,
+  inject,
+  getBpmnJS
+} from 'test/TestHelper';
+
+import {
+  createKeyEvent
+} from 'bpmn-js/test/util/KeyEvents';
+
+import {
+  query as domQuery
+} from 'min-dom';
+import Append from 'lib/features/append';
+
+import { forEach } from 'min-dash';
+
+import {
+  CreateAppendAnythingModule
+} from 'bpmn-js-create-append-anything';
+
+import diagramXML from './AppendNode.bpmn';
+
+describe('<AppendKeyboardBindings>', function() {
+
+  beforeEach(bootstrapModeler(diagramXML, {
+    additionalModules: [
+      Append,
+      CreateAppendAnythingModule
+    ]
+  }));
+
+
+  it('should include triggers inside editorActions', inject(function(editorActions) {
+
+    // given
+    const expectedActions = [
+      'appendNode'
+    ];
+    const actualActions = editorActions.getActions();
+
+    // then
+    expect(
+      expectedActions.every(action => actualActions.includes(action))
+    ).to.be.true;
+  }));
+
+
+  forEach([ 'a', 'A' ], function(key) {
+
+    it('should trigger append menu',
+      inject(function(keyboard, popupMenu, elementRegistry, selection) {
+
+        sinon.spy(popupMenu, 'open');
+
+        // given
+        const task = elementRegistry.get('Task');
+
+        selection.select(task);
+
+        const e = createKeyEvent(key);
+
+        // when
+        keyboard._keyHandler(e);
+
+        // then
+        expect(popupMenu.open).to.have.been.calledOnce;
+        expect(isMenu('bpmn-append')).to.be.true;
+      }));
+
+
+    it('should trigger create menu',
+      inject(function(keyboard, popupMenu) {
+
+        sinon.spy(popupMenu, 'open');
+
+        // given
+        const e = createKeyEvent(key);
+
+        // when
+        keyboard._keyHandler(e);
+
+        // then
+        expect(popupMenu.open).to.have.been.calledOnce;
+        expect(isMenu('bpmn-create')).to.be.true;
+      }));
+
+
+    it('should not trigger create or append menus',
+      inject(function(keyboard, popupMenu) {
+
+        sinon.spy(popupMenu, 'open');
+
+        // given
+        const e = createKeyEvent(key, { ctrlKey: true });
+
+        // when
+        keyboard._keyHandler(e);
+
+        // then
+        expect(popupMenu.open).to.not.have.been.called;
+      }));
+
+  });
+
+});
+
+
+// helpers //////////////////////
+function isMenu(menuId) {
+  const popup = getBpmnJS().get('popupMenu');
+  const popupElement = popup._current && domQuery('.djs-popup', popup._current.container);
+
+  return popupElement.classList.contains(menuId);
+}
