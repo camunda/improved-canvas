66bf93ba19da9d830abf0e91da1cd3b79fc55445
Philipp Fromme <philipp.fromme@camunda.com>
2024-04-25 13:33:09 +0200

    chore(create-pad): don't use overlays

--- test/spec/common/createPad/ErrorCreatePad.js
+++ test/spec/common/createPad/ErrorCreatePad.js
@@ -1,9 +1,9 @@
 import CreatePad from 'lib/common/createPad/CreatePad';
 
 export default class ErrorCreatePad extends CreatePad {
-  constructor(eventBus, overlays) {
-    super(eventBus, overlays);
+  constructor(canvas, eventBus) {
+    super(canvas, eventBus);
   }
 }
 
-ErrorCreatePad.$inject = [ 'eventBus', 'overlays' ];
+ErrorCreatePad.$inject = [ 'canvas', 'eventBus' ];
