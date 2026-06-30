aaebb4ee6ef9746d0b732e539c0857a52dbd235b
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2026-06-30 20:21:18 +0200

    chore: use karma-tldr-reporter

--- karma.config.js
+++ karma.config.js
@@ -33,7 +33,7 @@ module.exports = function(karma) {
       [ suite ]: [ 'webpack', 'env' ]
     },
 
-    reporters: [ 'progress' ].concat(coverage ? 'coverage' : []),
+    reporters: [ 'tldr' ].concat(coverage ? 'coverage' : []),
 
     coverageReporter: {
       reporters: [
